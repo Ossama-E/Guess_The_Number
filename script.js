15e91857d38cbeec793902a3948a0fcba60eb1ae
Ossama-E <ossama.elhe@gmail.com>
2023-05-16 22:12:44 -0400

    Overall code imorovements + added more hints objects

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
+
 // Function for the game
 const game = function () {
   // Generate a random number between 1 and 20
   const targetNum = Math.trunc(Math.random() * 20) + 1;
-  console.log(`TN is ${targetNum}`);
 
-  // Get the necessary DOM
+  // Get the necessary DOM elements
   const messageEl = document.querySelector('.message');
   const scoreEl = document.querySelector('.score');
   const guessEl = document.querySelector('.guess');
@@ -13,8 +13,11 @@ const game = function () {
   const numberEl = document.querySelector('.number');
   const checkBtnEl = document.querySelector('.check');
   const hintBtn = document.querySelector('.hint');
+
+  // Define hints and their conditions
   const hints = [
     { hint: 'The number is even.', condition: num => num % 2 === 0 },
+    { hint: 'The number is a prime number.', condition: num => isPrime(num) },
     { hint: 'The number is divisible by 3.', condition: num => num % 3 === 0 },
     {
       hint: 'The number is a perfect square.',
@@ -22,11 +25,36 @@ const game = function () {
     },
     { hint: 'The number is a multiple of 5.', condition: num => num % 5 === 0 },
     { hint: 'The number is greater than 10.', condition: num => num > 10 },
+    // Additional hints
+    { hint: 'The number is odd.', condition: num => num % 2 !== 0 },
+    { hint: 'The number is a multiple of 4.', condition: num => num % 4 === 0 },
+    {
+      hint: 'The number is a perfect cube.',
+      condition: num => Math.cbrt(num) % 1 === 0,
+    },
   ];
+
+  // Setup initial game state
   hintBtn.style.display = 'block';
   messageEl.textContent = 'Start guessing...';
   numberEl.textContent = '?';
   numberEl.style.color = '#222';
+
+  // Function to check whether a number is prime or not
+  function isPrime(num) {
+    if (num <= 1) {
+      return false;
+    }
+
+    // Check for divisibility from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 
   // Initialize the score
   let highScore = 0;
@@ -48,12 +76,14 @@ const game = function () {
     }
   };
 
+  // Function to get a random hint
   function getRandomHint(targetNum) {
     const eligibleHints = hints.filter(hint => hint.condition(targetNum));
     const randomIndex = Math.floor(Math.random() * eligibleHints.length);
     return eligibleHints[randomIndex]?.hint || 'No hint available.';
   }
 
+  // Function to show a hint
   function showHint() {
     const hint = document.createElement('h3');
     hint.classList.add('hint');
@@ -127,6 +157,7 @@ const game = function () {
 
 // Get the 'again' button DOM element
 const againBtnEl = document.querySelector('.btn.again');
+
 // Add event listener to the 'again' button
 againBtnEl.addEventListener('click', game);
 
